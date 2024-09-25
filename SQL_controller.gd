f3b434f7b4ac54052ac1ad46ce25e8394d65b351
ZeuwsFrade <mack.schukin@yandex.ru>
2024-09-25 17:53:24 +0300

    Movement_done

--- SQL_controller.gd
+++ SQL_controller.gd
@@ -10,3 +10,33 @@ func _ready():
 
 func _process(_delta):
 	pass
+
+
+func _on_create_table_button_down():
+	var table = {
+		"id": {"data_type": "int", "primary_key": true, "not_null": true, "auo_increment": true},
+		"name" : {"data_type": "text"},
+		"score" : {"data_type": "int"}
+	}
+	database.create_table("players", table)
+	pass # Replace with function body.
+
+
+func _on_insert_data_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_select_data_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_upload_data_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_delete_data_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_custom_select_button_down() -> void:
+	pass # Replace with function body.
--- TEsts/test_cahr.gd
+++ TEsts/test_cahr.gd
@@ -2,26 +2,32 @@ extends CharacterBody2D
 
 @export var tile_map: TileMapLayer
 @export var SPEED = 200
+const tile_width = 16
 var current_path: Array[Vector2i]
 
 func  _physics_process(delta):
 	if current_path.is_empty():
 		return
-	
 	var target_position = tile_map.map_to_local(current_path.front())
 	global_position = global_position.move_toward(target_position, SPEED*delta)
-	
 	if global_position == target_position:
-		current_path. pop_front()
+		current_path.pop_front()
 	pass
 
 func _unhandled_input(event):
-	var click_position = get_global_mouse_position()
-	if event.is_action_pressed("move_to"):
-		if tile_map.is_point_available(click_position):
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := Vector2(floor(input_dir.x), floor(input_dir.y)).normalized()
+	if direction:
+		#$"../Sprite2D".position = global_position + direction
+		if tile_map.is_point_available(global_position+direction*tile_width):
 			current_path = tile_map.astar.get_id_path(
 				tile_map.local_to_map(global_position),
-				tile_map.local_to_map(click_position)
-			).slice(1)
-	
-	
+				tile_map.local_to_map(global_position+direction*tile_width)
+				).slice(1)
+	#var click_position = get_global_mouse_position()
+	#if event.is_action_pressed("move_to"):
+		#if tile_map.is_point_available(click_position):
+			#current_path = tile_map.astar.get_id_path(
+				#tile_map.local_to_map(global_position),
+				#tile_map.local_to_map(click_position)
+			#).slice(1)
