39dec6df5193dfd55150ea9c48dfd817633a3d2c
HeFas <art.aparin2016@gmail.com>
2024-11-26 13:47:46 +0300

    attack_system_rework 0.6

--- scripts/characters_base.gd
+++ scripts/characters_base.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 class_name CharecterBase
 
-#Body является обязательным параметром! (его можно изменять, но название Body должно остаться)
 #is_exist - внутрення функция для проверки сломана ли конечность
 #chance - вероятность попадания по конечности random(0,1) <= chance
 #vital - жизненонеобходима ли конечность. Если true, то после уничтожения её - сущность умрёт
 #text - название на требуемом языке
+#check - функция вызывается специфической проверки возможности атаки конечности
 @export var Parts = { 
-	Body = {is_exist = true, chance = 0.75, vital = true, text = "Тело"},
+	Body = {is_exist = true, chance = 0.75, vital = true, text = "Тело", check = is_some_part_broke}, #Сделать фигню чтоб шанс мог изменятся от внешних источников (предметы, и т.д.).
 	Head = {is_exist = true, chance = 0.1, vital = true, text = "Голова"},
 	Arms = {is_exist = true, chance = 0.5, vital = false, text = "Руки"},
 	Legs = {is_exist = true, chance = 0.5, vital = false, text = "Ноги"},
@@ -30,6 +30,7 @@ signal OnDead()
 
 
 func _on_damaged(dmg, attacker):
+	
 	print(attacker.name, " нанёс ", dmg, " урона ", self.name)
 
 func _on_dead():
@@ -44,7 +45,7 @@ func _ready() -> void:
 func _deploy() -> void:
 	pass
 	
-func _can_attacked_body():#not include is_exist for body
+func is_some_part_broke():
 	if Parts.size() == 1:
 		return true
 	for i in Parts:
@@ -53,12 +54,11 @@ func _can_attacked_body():#not include is_exist for body
 	return false
 
 func _can_attacked_part(part):
-	if part != "Body":
+	if !Parts[part].has("check"):
 		if Parts[part].is_exist:
 			return true
 	else:
-		if _can_attacked_body():
-			return true
+		Parts[part]["check"].call()
 	return false
 
 func _on_part_broke(part):
